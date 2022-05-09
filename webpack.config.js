890620fd29bbce78125f07fb4c5e198b959b18ed
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-09 11:55:56 +0300

    Refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
-const path = require('path')
-
 module.exports = [
 	{
 		mode : 'none',
 		entry : './src/index.js',
 		output : {
-			path : path.join(__dirname, 'build'),
-			publicPath : '/build/',
+			publicPath : 'dist',
 			filename : 'index.bundle.js',
 		},
 		devServer : {
